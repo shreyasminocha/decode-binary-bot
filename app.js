c6259dd25d83102cf2eab31259cb764d4f1c9202
Shreyas Minocha <shreyasminocha7@gmail.com>
2018-06-24 00:47:03 +0530

    Add support for non-delimited strings

--- app.js
+++ app.js
@@ -21,15 +21,39 @@ const comments = client.CommentStream({
 const devUserName = 'ShreyasMinocha';
 const botNotice = `\n\n^I ^am ^a ^bot. ^If ^I'm ^doing ^something ^silly, ^please ^PM [^the ^guy ^who ^programmed ^me ](https://reddit.com/user/${devUserName})`;
 
-const isBinaryAscii = /^(?:[01]{8} )+$/g;
-
 comments.on('comment', (comment) => {
-    const body = comment.body.trim() + ' ';
+    const body = comment.body.trim();
+    let translated = decode(comment.body);
 
-    if (isBinaryAscii.test(body)) {
-        const bytes = body.match(/[01]{8}/gm);
-        const translated = bytes.reduce((accumulator, byte) => accumulator + String.fromCharCode(parseInt(byte, 2)),  '');
-
-        comment.reply(`That translates to: '${translated}'. ${botNotice}`);
+    if (decode(translated) !== '') {
+        comment.reply(`That translates to: "${translated}". ${botNotice}`);
     }
 });
+
+const isDecodable = /^(?:[01]{8} )+$/g;
+const isDecodableNoSpaces = /^[01]+$/g;
+const byteRegex = /[01]{8}/gm;
+
+function decode(string) {
+    string = string.trim();
+    let bytes = '';
+
+    if (isDecodable.test(string + ' ')) {
+        bytes = (string + ' ').match(byteRegex);
+    } else if(isDecodableNoSpaces.test(string) && string.length % 8 == 0) {
+        bytes = string.match(byteRegex);
+    }
+
+    if (bytes) {
+        return decodeBytes(bytes);
+    }
+
+    return '';
+}
+
+function decodeBytes(bytes) {
+    return bytes.reduce((accumulator, byte) => accumulator + String.fromCharCode(parseInt(byte, 2)),  '');
+}
+
+
+module.exports = {decode};
